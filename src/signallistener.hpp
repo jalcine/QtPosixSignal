b4adc8704ee56f6baf3622e7a045bbbb8efe8ef7
Jacky Alciné <me@jalcine.me>
2013-08-10 04:38:45 -0400

    Updated class definition.

--- src/signallistener.hpp
+++ src/signallistener.hpp
@@ -3,4 +3,17 @@
 
 #include <QtCore/QObject>
 
+namespace QtPosixSignal {
+  class SignalListenerPrivate;
+  class SignalListener : public QObject {
+    Q_OBJECT;
+    Q_DISABLE_COPY(SignalListener);
+    Q_DECLARE_PRIVATE(SignalListener);
+
+    public:
+      SignalListener(QObject* parent);
+      virtual ~SignalListener();
+  };
+}
+
 #endif
